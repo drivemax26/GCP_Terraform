37e4b6e1421f47e75d25019c9af8ee64d90481d4
Oleh Fadieiev <61756426+drivemax26@users.noreply.github.com>
2023-11-21 18:56:47 +0200

    Solution

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 
 output "instance_public_ip" {
-  value = google_compute_instance.my_instance.network_interface.0.access_config.*.nat_ip
+  value = google_compute_instance.my_instance.network_interface[0].access_config[*].nat_ip
 }
 
 output "instance_image_id" {
-  value = google_compute_instance.my_instance.boot_disk.0.initialize_params[0].image
+  value = google_compute_instance.my_instance.boot_disk[0].initialize_params[0].image
 }
 
 output "instance_type" {
@@ -12,11 +12,11 @@ output "instance_type" {
 }
 
 output "instance_network" {
-  value = google_compute_instance.my_instance.network_interface.0.network
+  value = google_compute_instance.my_instance.network_interface[0].network
 }
 
 output "instance_subnet" {
-  value = google_compute_instance.my_instance.network_interface.0.subnetwork
+  value = google_compute_instance.my_instance.network_interface[0].subnetwork
 }
 
 output "instance_zone" {
